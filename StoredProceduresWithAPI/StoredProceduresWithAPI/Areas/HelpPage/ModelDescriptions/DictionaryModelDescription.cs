ce6707d85cac4fcd5ce05b732b6b340a3dac5e72
Markkimaathi <markangelkimathi@gmail.com>
2024-08-14 11:41:09 +0300

    files

---
+++ StoredProceduresWithAPI/StoredProceduresWithAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StoredProceduresWithAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ StoredProceduresWithAPI/StoredProceduresWithAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StoredProceduresWithAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
