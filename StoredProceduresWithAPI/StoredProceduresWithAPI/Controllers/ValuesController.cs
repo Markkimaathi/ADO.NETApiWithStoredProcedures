cb46a9ec7b6820d3957448fd66d80c50d8d468dd
Markkimaathi <markangelkimathi@gmail.com>
2024-08-14 12:17:41 +0300

    API add

--- StoredProceduresWithAPI/StoredProceduresWithAPI/Controllers/ValuesController.cs
+++ StoredProceduresWithAPI/StoredProceduresWithAPI/Controllers/ValuesController.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using StoredProceduresWithWebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace StoredProceduresWithAPI.Controllers
 {
     public class ValuesController : ApiController
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["webapi_conn"].ConnectionString);
+        Employee emp = new Employee();
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -22,8 +28,33 @@ namespace StoredProceduresWithAPI.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody] string value)
+        public string Post(Employee employee)
         {
+            string msg = "";
+            if (employee != null)
+            {
+                SqlCommand cmd = new SqlCommand("usp_AddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Age", employee.Age);
+                cmd.Parameters.AddWithValue("@Active", employee.Active);
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (i > 0)
+                {
+                    msg = "Data has been inserted";
+                }
+                else
+                {
+                    msg = "Error";
+                }
+
+            }
+            return msg;
+
         }
 
         // PUT api/values/5
